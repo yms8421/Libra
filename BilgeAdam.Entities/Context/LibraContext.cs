9bd7ae01d3ab2b2b5102d360416ec143cf90ac86
yms8421 <yms8421@outlook.com>
2019-06-08 12:27:16 +0300

    Project start up

---
+++ BilgeAdam.Entities/Context/LibraContext.cs
@@ -0,0 +1,13 @@
+﻿using BilgeAdam.Entities.Models;
+using System.Data.Entity;
+
+namespace BilgeAdam.Entities.Context
+{
+    public class LibraContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Shelve> Shelves { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+    }
+}
