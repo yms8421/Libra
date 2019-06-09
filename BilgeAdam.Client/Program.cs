d50de34b349e765ec792d02c7f6850d77f0a69f2
Mehmet Ilhan <mehmetilhan@live.com>
2019-06-09 09:49:13 +0300

    Program Application.Run Login Ekranina Ayarlandı. ;)

--- BilgeAdam.Client/Program.cs
+++ BilgeAdam.Client/Program.cs
@@ -16,7 +16,7 @@ namespace BilgeAdam.Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmLogin());
         }
     }
 }
